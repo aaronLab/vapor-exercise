4b8e8abfecde6bf1a3bf78dda45b86b85d280f66
Aaron Lee <aaronlab.net@gmail.com>
2021-01-03 16:12:59 +0900

    Hello Vapor

---
+++ hello vapor/Sources/App/routes.swift
@@ -0,0 +1,20 @@
+import Vapor
+
+func routes(_ app: Application) throws {
+    app.get { req in
+        return "It works!"
+    }
+
+    app.get("hello") { req -> String in
+        return "Hello, world!"
+    }
+    
+    app.get("hello", "vapor") { req -> String in
+        return "Hello, Vapor!"
+    }
+    
+    app.get("hello", ":name") { req -> String in
+        let name = req.parameters.get("name")!
+        return "Hello, \(name)!"
+    }
+}
