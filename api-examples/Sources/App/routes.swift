d6310fc242cf54e0b60ab318eb30bf271ef99fc6
Aaron Lee <aaronlab.net@gmail.com>
2021-01-04 22:54:38 +0900

    Any Params + Catch All

--- api-examples/Sources/App/routes.swift
+++ api-examples/Sources/App/routes.swift
@@ -6,9 +6,17 @@ func routes(_ app: Application) throws {
         return "\"get\" works properly"
     }
     
-    app.get("api", "get", ":param") { req -> String in
-        let param = req.parameters.get("param")
-        return "\"get with a parameter\" works properly\nparam: \(param!)"
+    app.get("api", "get", "params", ":param") { req -> String in
+        let param = req.parameters.get("param")!
+        return "\"get with a parameter\" works properly\nparam: \(param)"
+    }
+    
+    app.get("api", "get", "any", "*", "params") { req -> String in
+        return "anyparams"
+    }
+    
+    app.get("api", "get", "catchall", "**") { req -> String in
+        return "catchall"
     }
     
 }
