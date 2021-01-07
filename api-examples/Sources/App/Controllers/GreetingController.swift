a9caac1037790db8ff6bca7d6db6fbae001d8280
Aaron Lee <aaronlab.net@gmail.com>
2021-01-07 21:50:40 +0900

    controller

--- api-examples/Sources/App/Controllers/GreetingController.swift
+++ api-examples/Sources/App/Controllers/GreetingController.swift
@@ -1,8 +1,19 @@
 //
-//  File.swift
+//  GreetingController.swift
 //  
 //
 //  Created by Aaron Lee on 2021-01-07.
 //
 
 import Foundation
+import Vapor
+
+func greetingController(_ app: Application) throws {
+    
+    app.post("api", "greeting") { req -> HTTPStatus in
+        let greeting = try req.content.decode(Greeting.self)
+        print(greeting.hello)
+        return HTTPStatus.ok
+    }
+    
+}
