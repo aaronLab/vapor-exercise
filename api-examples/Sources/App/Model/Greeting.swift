b1f354af3da3db411c01373f23a97bcc5bb1d743
Aaron Lee <aaronlab.net@gmail.com>
2021-01-07 21:50:49 +0900

    model

--- api-examples/Sources/App/Model/Greeting.swift
+++ api-examples/Sources/App/Model/Greeting.swift
@@ -1,8 +1,13 @@
 //
-//  File.swift
+//  Greeting.swift
 //  
 //
 //  Created by Aaron Lee on 2021-01-07.
 //
 
 import Foundation
+import Vapor
+
+struct Greeting: Content {
+    var hello: String
+}
